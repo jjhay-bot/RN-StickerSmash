b4af57acdf952f057e53088f805ee9b23a587a8f
jhay <79243795+jjhay-bot@users.noreply.github.com>
2023-08-08 23:09:52 +0800

    update phone StatusBar
    update phone StatusBar = light

--- App.js
+++ App.js
@@ -125,7 +125,7 @@ export default function App() {
           <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
         </EmojiPicker>
 
-        <StatusBar style="auto" />
+        <StatusBar style="light" />
       </View>
       {/* </ImageBackground> */}
     </GestureHandlerRootView>
